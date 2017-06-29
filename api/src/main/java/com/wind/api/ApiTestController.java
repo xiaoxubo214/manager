43851b707415be390da993da84bfa3d1e51dd20e
xiaoxubo <xiaoxubo@vtstar.net>
2017-06-29 10:14:02 +0800

    add file

---
+++ api/src/main/java/com/wind/api/ApiTestController.java
@@ -0,0 +1,39 @@
+package com.wind.api;
+
+import com.wind.annotation.IgnoreAuth;
+import com.wind.entity.UserEntity;
+import com.wind.utils.R;
+import com.wind.annotation.LoginUser;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * API测试接口
+ *
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-23 15:47
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiTestController {
+
+    /**
+     * 获取用户信息
+     */
+    @GetMapping("userInfo")
+    public R userInfo(@LoginUser UserEntity user){
+        return R.ok().put("user", user);
+    }
+
+    /**
+     * 忽略Token验证测试
+     */
+    @IgnoreAuth
+    @GetMapping("notToken")
+    public R notToken(){
+        return R.ok().put("msg", "无需token也能访问。。。");
+    }
+}
---
+++ common/src/main/java/com/wind/dao/SysLogDao.java
@@ -0,0 +1,14 @@
+package com.wind.dao;
+
+import com.wind.entity.SysLogEntity;
+
+/**
+ * 系统日志
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-08 10:40:56
+ */
+public interface SysLogDao extends BaseDao<SysLogEntity> {
+	
+}
